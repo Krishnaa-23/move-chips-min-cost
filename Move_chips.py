0bec4f6a2ef70f7187611ed634211c1b523669e5
Krishna <krishnaaa2304@gmail.com>
2025-08-10 23:05:18 +0530

    Add files via upload

---
+++ Move_chips.py
@@ -0,0 +1,15 @@
+def minCostToMoveChips(position):
+    even_count = 0
+    odd_count = 0
+    
+    for p in position:
+        if p % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+    
+    return min(even_count, odd_count)
+
+# Example test
+positions = [1, 2, 3]
+print("Minimum cost:", minCostToMoveChips(positions))
